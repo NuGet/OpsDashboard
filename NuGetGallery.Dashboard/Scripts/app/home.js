9a17aba353d4f57bd9f45a5f8a822d03e276813e
anurse <andrew@andrewnurse.net>
2013-02-09 13:44:10 -0800

    Added snazzy "as of" last updated message

--- NuGetGallery.Dashboard/Scripts/app/home.js
+++ NuGetGallery.Dashboard/Scripts/app/home.js
@@ -1,12 +1,27 @@
 ﻿(function ($, window, undefined) {
+    var refreshInterval = 5 * 60 * 1000;
+    var tickInterval = 1 * 1000;
+
     function EnvironmentViewModel(accessible, name, description, url) {
         var self = {};
 
+        self.lastUpdated = ko.observable(Date.now());
         self.accessible = ko.observable(accessible);
         self.name = ko.observable(name);
         self.description = ko.observable(description);
         self.url = ko.observable(url);
         self.loading = ko.observable(true);
+
+        var ticker = ko.observable(0);
+        self.lastUpdatedString = ko.computed(function () {
+            if (ticker() > 0) {
+                return $.timeago(self.lastUpdated());
+            }
+            return '';
+        });
+        setInterval(function () {
+            ticker(ticker() + 1)
+        }, tickInterval);
 
         self.icon = ko.computed(function () {
             if (self.loading()) { // Not actually false, thus must be "falsey" (null/undefined)
@@ -21,6 +36,7 @@
         });
 
         self.load = function () {
+            self.loading(true);
             $.getJSON('/api/v1/environments/' + self.name(), function (data, status, xhr) {
                 if (data) {
                     self.accessible(data.AccessibleFromDashboard);
@@ -31,6 +47,7 @@
                     alert('Error loading data from server: ' + status);
                 }
                 self.loading(false);
+                self.lastUpdated(Date.now());
             });
         }
 
@@ -55,6 +72,7 @@
             $.getJSON('/api/v1/environments', function (data, status, xhr) {
                 if (data) {
                     var currentRow = null;
+                    self.rows.removeAll();
                     for (var i = 0; i < data.length; i++) {
                         if (currentRow == null) {
                             currentRow = new RowViewModel();
@@ -72,14 +90,16 @@
                 }
                 self.loading(false);
             });
+            setTimeout(self.load, refreshInterval);
         }
 
         return self;
     }
 
     $(function () {
+        $('time').timeago();
         var viewModel = new PageViewModel();
         ko.applyBindings(viewModel);
-        setTimeout(viewModel.load(), 0);
+        setTimeout(viewModel.load, 0);
     });
 })(jQuery, window);
